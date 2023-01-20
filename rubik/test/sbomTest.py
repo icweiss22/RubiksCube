6f304aa2e2fe7c1b302f102d80117fcf0429f9f0
Isaac Weiss <icw0001@auburn.edu>
2023-01-20 11:47:44 +0100

    increment 0 is 'done'

---
+++ rubik/test/sbomTest.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 20, 2023
+
+@author: isaacweiss
+'''
+import unittest
+import app
+
+
+class SbomTest(unittest.TestCase):
+
+
+    def test_sbom_100_ShouldReturnArthurName(self):
+        myName = 'jdg0078'
+        result = app._getAuthor('../../')
+        resultingAuthorName = result['author']
+        self.assertEqual(resultingAuthorName, myName)
